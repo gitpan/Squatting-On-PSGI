da58e0f4e301e9d88424828f04a9560250775fc9
John Beppu <beppu@cpan.org>
2009-10-13 22:05:00 +0000

    Import of BEPPU/Squatting-On-PSGI-0.01 from CPAN.
    gitpan-cpan-distribution: Squatting-On-PSGI
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BEPPU/Squatting-On-PSGI-0.01.tar.gz
    gitpan-cpan-author:       BEPPU
    gitpan-cpan-maturity:     released

---
+++ t/lib/Hello.pm
@@ -0,0 +1,48 @@
+{
+    package Hello;
+    use base qw(Squatting);
+}
+
+{
+    package Hello::Controllers;
+    use Squatting ':controllers';
+
+    my $home = sub {
+        my $self = shift;
+        $self->v->{title} = "Hello world";
+        $self->v->{message} = "Your name: " . $self->input->{name};
+        $self->render('home');
+    };
+    our @C = (
+        C( Home => ['/'],
+           get => $home,
+           post => $home ),
+        C( Multi => ['/multi'],
+           get => sub {
+               my $self = shift;
+               $self->{v}->{message} = join ",", @{$self->input->{'q'}};
+               $self->render('home');
+           } ),
+    );
+}
+{
+    package Hello::Views;
+    use Squatting ':views';
+
+    our @V = (
+        V(
+            'html',
+            layout => sub {
+                my($self, $v, $content) = @_;
+                "<html><head><title>$v->{title}</title></head>".
+                "<body>$content</body></html>";
+            },
+            home => sub {
+                my($self, $v) = @_;
+                "<h1>$v->{message}</h1>",
+            },
+        ),
+    );
+}
+
+1;
